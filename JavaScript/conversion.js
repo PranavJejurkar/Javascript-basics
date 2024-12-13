4c1d5968edcfb71d95849ea57598f445489943ae
Pranav Jejurkar <95302595+PranavJejurkar@users.noreply.github.com>
2024-12-13 11:28:13 +0000

    conversion

---
+++ JavaScript/conversion.js
@@ -0,0 +1,14 @@
+let number =3
+console.log(typeof number);
+
+// 
+let string = String(number)
+console.log(typeof string)
+// 
+let a =true
+console.log(Number(a))
+// 
+let p;
+console.log(typeof p)
+let p1=null
+console.log(typeof p1)
